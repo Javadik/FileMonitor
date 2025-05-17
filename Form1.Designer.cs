fc9be1c47465e0208de6085bbb506e485880683b
VadimR <VadimR@Powder>
2025-05-17 10:59:32 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace FileMonitor
+{
+    partial class FileMonitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbCurplay = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbLogger = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(100, 48);
+            this.btStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(100, 28);
+            this.btStart.TabIndex = 0;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(263, 48);
+            this.btStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(100, 28);
+            this.btStop.TabIndex = 1;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 113);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.cbCurplay);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.cbLogger);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1277, 442);
+            this.splitContainer1.SplitterDistance = 180;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(617, 85);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "копировать в";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(611, 112);
+            this.comboBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(523, 24);
+            this.comboBox4.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "cur_playing";
+            // 
+            // cbCurplay
+            // 
+            this.cbCurplay.FormattingEnabled = true;
+            this.cbCurplay.Location = new System.Drawing.Point(16, 112);
+            this.cbCurplay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbCurplay.Name = "cbCurplay";
+            this.cbCurplay.Size = new System.Drawing.Size(523, 24);
+            this.cbCurplay.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(617, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "копировать в";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(611, 37);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(523, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "папка логгера";
+            // 
+            // cbLogger
+            // 
+            this.cbLogger.FormattingEnabled = true;
+            this.cbLogger.Location = new System.Drawing.Point(16, 37);
+            this.cbLogger.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbLogger.Name = "cbLogger";
+            this.cbLogger.Size = new System.Drawing.Size(523, 24);
+            this.cbLogger.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1277, 257);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(449, 70);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FileMonitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 554);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FileMonitor";
+            this.Text = "FileMonitor";
+            this.Load += new System.EventHandler(this.FileMonitor_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbLogger;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbCurplay;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileMonitor
+{
+    public partial class FileMonitor : Form
+    {
+        string monitorDir = "E:\\testFiles";//@"\\192.168.1.72\1hdplay\files";
+        string xmlFile = "C:\\Users\\VadimR\\GoogleD_VCh\\Prog\\FileMonitor\\cur_playing.xml";  // "C:\\Users\\Vadim\\source\\repos\\FileMonitor\\cur_playing.xml";
+        string audioPath = "E:\\testFiles3\\";
+        Thread loggerThread;
+        Logger logger;
+        List<string> curList;
+        List<string> audioList = new List<string>();
+        public FileMonitor()
+        {
+            InitializeComponent();
+
+            logger = new Logger(monitorDir);
+            loggerThread = new Thread(new ThreadStart(logger.Start));
+            loggerThread.Start();
+            
+
+
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            logger.Start();
+            splitContainer1.Panel1.Enabled = false;
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            logger.Stop();
+            splitContainer1.Panel1.Enabled = true;
+            Thread.Sleep(1000);
+        }
+
+        public List<string> GetFilesinXML(String fl)
+        {
+            string pattern = @"<FILE_NAME>(.*?)<\/FILE_NAME>";
+            string input;
+            List<string> list = new List<string>();
+            var fs = new FileStream(fl, FileMode.Open, FileAccess.Read,
+                                      FileShare.ReadWrite | FileShare.Delete);
+            using (StreamReader reader = new StreamReader(fs, Encoding.UTF8))
+            {
+                input = reader.ReadToEnd();
+            }
+
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(input);
+        // System.Console.WriteLine(match.Groups[1].Value);
+            for (int i = 0; i < matches.Count; i++)
+            {
+                string st = matches[i].Groups[1].Value;
+                list.Add(st);
+            }
+            return list;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            curList = GetFilesinXML(xmlFile);
+            if (curList.Count() != 0)
+            {
+                if (!audioList.Intersect(curList).Any()) //if not all curList in audioList
+                {
+
+                    foreach (var file in curList) {
+                        if (!audioList.Contains(file))
+                        {
+                            if (File.Exists(file))
+                            {
+                                File.Copy(file, audioPath + Path.GetFileName(file), true);
+                                audioList.Add(file);
+                            }
+                            else { richTextBox1.AppendText($"такого файла не существует: {file}"); }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FileMonitor_Load(object sender, EventArgs e)
+        {
+            logger.Stop();
+        }
+    }
+}
